074f3c94e3754dc1a22426d656831850101f61ca
truehello <trudelmarc@gmail.com>
2019-08-22 16:14:17 -0700

    debug cms access

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -6,6 +6,7 @@ import Menu from "../components/menu.component"
 const NotFoundPage = () => (
   <AppLayout>
     <h1>Articles</h1>
+    <p>This is a collection or articles that are originally meant to be helpful for me to reference back to. If they are helpful to anyone else, all the better. </p>
     <Menu />
   </AppLayout>
 )
