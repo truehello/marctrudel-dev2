b3201b4f5a90711094d79d8faec85ba4c86fbe17
truehello <trudelmarc@gmail.com>
2019-09-10 13:54:11 -0700

    updated react

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+//import Particles from 'react-particles-js'
 
 import { Header } from "../components/header"
 import Footer from "../components/footer"
-//import Transition from "../components/transitions";
 
+
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 80,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       }
+//     }
+//   }
+// }
 // Global application wrapper
 export const Layout = ({ children, location }) => (
   <StaticQuery
@@ -22,6 +34,7 @@ export const Layout = ({ children, location }) => (
     render={data => {
       return (
         <>
+        {/* <Particles className='particles' params={particlesOptions} /> */}
           <Header siteTitle={data.site.siteMetadata.title} />
 
           <main className="container w-full md:max-w-3xl mx-auto pt-20 px-4 lg:px-0 flex-grow">
@@ -29,6 +42,8 @@ export const Layout = ({ children, location }) => (
           </main>
 
           <Footer siteAuthor={data.site.siteMetadata.author} />
+
+          
         </>
       )
     }}
