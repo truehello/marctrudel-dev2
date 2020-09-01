076df40dd4462888e15c79204c56ccd46bb8e911
truehello <trudelmarc@gmail.com>
2020-09-01 14:54:57 -0700

    new index.js

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+
+
+const Articles = () => (
+  
+  <Layout>
+    <h1>About</h1>
+    <p>This is a collection or articles that are originally meant to be helpful for me to reference back to. If they are helpful to anyone else, all the better. </p>
+    
+  </Layout>
+  
+)
+
+export default Articles
